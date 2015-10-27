fd50629a8d72133d1eef5901e57c6f8248601749
Brendan Doyle <bdoyle522@gmail.com>
2015-10-27 11:54:11 -0500

    today's project completed

--- practice.js
+++ practice.js
@@ -1,19 +1,19 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer
+      //The purpose of the this keyword is to point to an object without making it specific to one object
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      //
 
   // 3) What is the difference between call and apply?
 
-      //Answer
+      //Call takes in specific arguments while apply can take in object arguments?
 
   // 4) What does .bind do?
 
-      //Answer
+      //.bind returns a function for use
 
 
 //Next Problem
@@ -21,28 +21,48 @@
 //Create an object called user which has the following properties.
   //username --> which is a string
   //email --> which is a string
-  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+  //getUsername --> which is a function that returns the current object's username property. 
+  //*Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+var user = {
+  username: 'bdoyle',
+  email: 'bdoyle522@gmail.com',
+  getUsername: function(){
+    return this.username;
+  }
+}
+
+
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
 
-  //Function Invocations Here
+function Car (make,model,year){
+    this.make = make,
+    this.model = model,
+    this.year = year
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
+
+Car.prototype.moveCar = function(){
+    if(this.move === 10){return this.move+10;}
+    return this.move = 10;
+}
+
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the write object (prius vs mustang).
-
+//Hint, you'll need to write a moveCar function which is added to every object that is being returned 
+//from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're 
+//invoking moveCar on the write object (prius vs mustang).
 
 
 //Continuation of previous problem
@@ -51,10 +71,12 @@ var getYear = function(){
   return this.year;
 };
 
-//Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
+//Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax 
+//that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. 
+//*Don't add getYear as a property on both objects*.
 
-  //Code Here
-
+console.log(getYear.apply(prius));
+console.log(getYear.apply(mustang));
 
 
 //New Problem
@@ -71,14 +93,14 @@ var getUsername = function(){
   console.log(this.username);
 };
 
-setTimeout(getUsername, 5000);
+setTimeout(getUsername.apply(user), 5000);
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 
-  //Answer Here
+  //It will give a value of undefined
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
-  //Answer Here
+  //the global scope, (window)
 
 //Fix the setTimeout invocation so that the user object will be the focal object when getUsername is ran.
